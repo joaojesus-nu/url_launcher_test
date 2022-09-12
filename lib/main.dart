9a3a09824d53ea1dd51fdb49ba84090585335ff1
Alan Lamb <alan.lamb@alan.lamb>
2022-09-12 10:30:40 -0300

    add ussd_service lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:ussd_service/ussd_service.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:sim_data/sim_data.dart';
 
-final _url = Uri(scheme: 'tel', path: '*#06#');
+final _url = Uri(scheme: 'tel', path: Uri.encodeComponent('*#06#'));
 
 void main() => runApp(
       const MaterialApp(
@@ -17,8 +20,24 @@ void main() => runApp(
     );
 
 Future<void> _launchUrl() async {
+  await Permission.phone.request();
+  if (!await Permission.phone.isGranted) {
+    throw Exception("permission missing");
+  }
+  SimData simData = await SimDataPlugin.getSimData();
+
+  try {
+    String? response = await UssdService.makeRequest(
+        simData.cards.first.subscriptionId, "*#06#");
+    print(response);
+  } catch (e) {
+    print('erro');
+    print(e);
+  }
+
+  print('eae');
   print(_url);
-  if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
+  if (!await launchUrl(_url, mode: LaunchMode.externalNonBrowserApplication)) {
     throw 'Could not launch $_url';
   }
 }
